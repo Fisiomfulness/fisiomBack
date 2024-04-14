79917a8f66b28922666c8bcd9a2085b08cca5540
jackira01 <s4bu3z0@gmail.com>
2024-04-14 12:11:21 -0500

    update controller login, model user, adminMiddleware

--- src/controllers/login/login.js
+++ src/controllers/login/login.js
@@ -9,56 +9,26 @@ const login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      res.status(201).json({ message: 'Usuario no encontrado' });
+      res.status(401).json({ message: 'Usuario no encontrado' });
     } else {
       const coincidePass = await verifyHashedData(password, user.password);
 
       if (!coincidePass) {
-        res.status(201).json({ message: 'Password incorrecto' });
+        res.status(401).json({ message: 'Password incorrecto' });
       } else {
-        const token = jwt.sign(
-          { userId: user._id, role: user.role },
-          JWT_SECRET,
-          { expiresIn: '1h' },
-        );
+        let userForToken = { userId: user._id, role: user.role };
 
-        /*
-      - Colocar estos parametros dentro de cookie en un obj como tercer parametro para producion
-      httpOnly: true, // Evitar acceso desde JavaScript del cliente
-      secure: true, // Usar solo sobre HTTPS (si es posible)
-      maxAge: 1000 * 60 * 60, // Una hora en milisegundos
-      sameSite: 'strict', // Mitigar vulnerabilidades CSRF
-    */
-        res.cookie('token', token, {
-          httpOnly: true,
-          secure: true,
-          maxAge: 1000 * 60 * 60,
-          sameSite: 'strict',
-        });
+        const token = jwt.sign(userForToken, JWT_SECRET, { expiresIn: '1h' });
 
-        return res.status(200).json({ message: 'Sesión iniciada con éxito' });
+        res.setHeader('Authorization', `Bearer ${token}`);
+
+        return res.status(201).json({ message: 'Sesión iniciada con éxito' });
       }
     }
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(500).send(error.message);
   }
 };
-
-// const login = async (req, res) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   const { email, password } = req.body;
-//   try {
-//     const user = await User.findOne({email, password });
-//     if (user) {
-//       const token = jwt.sign({ userId: user._id, role: user.role }, JWT_secret, { expiresIn: '1h' });
-//       return res.status(200).json({user, token});
-//     } else {
-//       return res.status(401).json({ message: 'Usuario no encontrado' });
-//     }
-//   } catch (error) {
-//     return res.status(400).send(error.message);
-//   }
-// }
 
 module.exports = {
   login,
