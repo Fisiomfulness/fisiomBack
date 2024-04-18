1f13c18bb69fafcd5a2978c369948d0e847a2c8d
Kevin Mamani <kevin72309@gmail.com>
2024-04-18 19:14:04 -0300

    feat: get comments with offset and limit to use with client scroll

--- src/controllers/comment/getCommentBlog.js
+++ src/controllers/comment/getCommentBlog.js
@@ -1,12 +1,34 @@
 const Comment = require('../../models/Comment');
+const Blog = require('../../models/Blog');
+
+const LIMIT_COMMENTS = 30;
 
 const getCommentBlog = async (req, res) => {
   const blog_id = req.params.id;
+  if (!(await Blog.findById(blog_id)))
+    return res.status(404).json({ message: 'blog not found' });
+
+  const { offset = 0, limit = LIMIT_COMMENTS } = req.query;
+  const offsetInt = parseInt(offset);
+  const limitInt = parseInt(limit);
+
+  if (!Number.isInteger(offsetInt) || !Number.isInteger(limitInt))
+    return res.status(400).json({ message: 'offset and limit must be integers' });
+
+  if (limitInt > LIMIT_COMMENTS)
+    return res.status(400).json({ message: 'limit exceeded' });
+
   try {
-    const comments = await Comment.find({ blog: blog_id }).populate('sender', "name image");
-    return res.status(200).json({ comments });
+    const totalComments = await Comment.countDocuments({ blog: blog_id });
+    const comments = await Comment.find({ blog: blog_id })
+      .sort({ createdDate: -1 })
+      .skip(offsetInt)
+      .limit(limitInt)
+      .populate('sender', 'name image');
+
+    res.status(200).json({ comments, totalComments });
   } catch (error) {
-    return res.status(404).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
