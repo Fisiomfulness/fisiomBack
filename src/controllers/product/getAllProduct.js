dab89e84e580e24a0188d7f348e3ea6953fcff31
brunosarti83 <brunosarti.bs@gmail.com>
2024-04-19 15:22:57 -0300

    WIP(paginate getProducts)

--- src/controllers/product/getAllProduct.js
+++ src/controllers/product/getAllProduct.js
@@ -3,21 +3,20 @@ const Product = require('../../models/Product');
 
 
 const getAllProduct = async (req, res) => {
-  const { title } = req.query;
   try {
-    const products = await Product.find({ status: true }).populate(
+    const { title } = req.query;
+
+    const query = { status: true };
+    if (title) {
+      query.title = title;
+    };
+    const products = await Product.find(query).populate(
       'category',
       'name'
     );
+    
 
-    if (!title) return res.status(200).json({ products });
-
-    const productFilter = products.filter((product) =>
-      product.title.toLowerCase().includes(title.toLowerCase())
-    );
-    if (!productFilter.length) throw new Error('no product found');
-
-    return res.status(200).json({ productFilter });
+    return res.status(200).json({ products });
   } catch (error) {
     return res.status(404).json({ message: error.message });
   }
