95ff8e559d7aac903e17c93a4d360555a0c00eb1
brunosarti83 <brunosarti.bs@gmail.com>
2024-04-19 20:01:23 -0300

    GetProducts(Added Pagination and backend filtering)

--- src/controllers/product/getAllProduct.js
+++ src/controllers/product/getAllProduct.js
@@ -4,19 +4,43 @@ const Product = require('../../models/Product');
 
 const getAllProduct = async (req, res) => {
   try {
-    const { title } = req.query;
+    const { 
+      page = 1,
+      limit = 10,
+      search = '',
+      categoryId = ''
+    } = req.query;
 
+    const pageInt = parseInt(page);
+    const limitInt = parseInt(limit);
+
+    if (!Number.isInteger(pageInt) || !Number.isInteger(limitInt)) {
+      return res.status(400).json({ message: 'page and limit must be integers' });
+    }
+
+    const skipIndex = (pageInt - 1) * limitInt;
     const query = { status: true };
-    if (title) {
-      query.title = title;
-    };
-    const products = await Product.find(query).populate(
-      'category',
-      'name'
-    );
-    
 
-    return res.status(200).json({ products });
+    if (search.trim() !== '') {
+      query.name = { $regex: new RegExp(search, 'i') }
+    };
+
+    
+    if (categoryId.trim() !== '') {
+      query.category = categoryId
+    }
+
+    const products = await Product.find(query)
+    .skip(skipIndex)
+    .limit(limitInt)
+    .populate({
+      path: 'category',
+      select: 'name'
+    });
+    
+    const totalProducts = await Product.countDocuments(query);
+    const totalPages = Math.ceil(totalProducts / limitInt);
+    return res.status(200).json({ products, page: pageInt, totalPages });
   } catch (error) {
     return res.status(404).json({ message: error.message });
   }
