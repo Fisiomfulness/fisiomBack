21a8cc5a814a7feaa60a944c16d3b2341ba0ff7f
Jackira01 <s4bu3z0@gmail.com>
2024-04-24 18:36:46 -0500

    modularizing and refactor "createProfessionalScore"

---
+++ src/controllers/professional/createProfessionalScore.js
@@ -0,0 +1,46 @@
+const Profesional = require('../../models/Profesional');
+const ProfessionalScore = require('../../models/ProfessionalScore');
+
+const createProfessionalScore = async (req, res) => {
+  const { _user, _profesional, name, description, score } = req.body;
+  const profesional = await Profesional.findById(_profesional);
+
+  try {
+    const newProfessionalScore = new ProfessionalScore({
+      _profesional,
+      _user,
+      name,
+      description,
+      score,
+    });
+
+    let totalComments = profesional.averageScore.totalComments + 1;
+    let totalScore = profesional.averageScore.totalScore + score;
+
+    const average = Math.round(totalScore / totalComments);
+
+    const newProfesional = {
+      averageScore: {
+        totalScore,
+        totalComments,
+        average,
+      },
+    };
+
+    await Profesional.findOneAndUpdate({ _id: _profesional }, newProfesional);
+
+    const savedProfessionScore = await newProfessionalScore.save();
+
+    profesional.professionalScore = profesional.professionalScore.concat(
+      savedProfessionScore._id,
+    );
+
+    profesional.save();
+
+    res.status(201).send(savedProfessionScore);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports = createProfessionalScore;
