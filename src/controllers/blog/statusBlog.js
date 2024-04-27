12dc242ae53ee767e099e3f0a62f4516634ac1d1
Kevin Mamani <kevin72309@gmail.com>
2024-04-26 22:32:43 -0300

    fix: boolean status validation

--- src/controllers/blog/statusBlog.js
+++ src/controllers/blog/statusBlog.js
@@ -6,11 +6,10 @@ const statusBlog = async (req, res) => {
   const { id } = req.params;
   const { status } = req.body;
 
+  if (typeof status !== 'boolean') throw new BadRequestError('invalid status');
   const blog = await Blog.findById(id);
   if (!blog) throw new NotFoundError('blog not found');
-  if (!status || typeof status !== 'boolean')
-    throw new BadRequestError('invalid status');
-
+  
   blog.status = status;
   await blog.save();
 
