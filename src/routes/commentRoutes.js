d8e5277527fea2e11a2dabcf06b74e0ef8786d2d
Kevin Mamani <kevin72309@gmail.com>
2024-04-30 09:23:32 -0300

    feat: role based protection comment routes

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -10,14 +10,28 @@ const { errorMiddleware } = require('../middleware/errorMiddleware');
 const { validationMiddleware } = require('../middleware/validationMiddleware');
 const { commentSchema } = require('../util/validationSchemas');
 
+const roles = require('../util/roles');
+const authAll = require('../middleware/authAll');
+const permit = require('../middleware/rolesMiddleware');
 const router = Router();
 
 router.get('/', asyncHandler(getComment));
 router.get('/blog/:id', asyncHandler(getCommentBlog));
 
-router.post('/create', validationMiddleware(commentSchema), asyncHandler(createComment));
+router.use(authAll);
 
-router.delete('/delete/:id', asyncHandler(deleteComment));
+router.post(
+  '/create',
+  permit(roles.USER, roles.SUPER_ADMIN),
+  validationMiddleware(commentSchema),
+  asyncHandler(createComment)
+);
+
+router.delete(
+  '/delete/:id',
+  permit(roles.ADMIN, roles.SUPER_ADMIN),
+  asyncHandler(deleteComment)
+);
 
 router.use(errorMiddleware);
 
