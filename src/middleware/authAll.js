b16098be0d74ef610deb4aaa1adb6e1cda8e7967
jackira01 <s4bu3z0@gmail.com>
2024-04-30 12:53:13 -0500

    add authAll

---
+++ src/middleware/authAll.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/envConfig');
+
+const authAll = (req, res, next) => {
+  const token = req.cookies['accessToken'];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.clearCookie('accessToken');
+      return res.status(401).json({ message: 'Invalid token or expired' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = authAll;
