a92b49104ad3d740f35e99e6de60e99fa9c8fe2b
jackira01 <s4bu3z0@gmail.com>
2024-05-02 14:31:47 -0500

    add promise funcion

--- src/controllers/login/register/ProfesionalRegister.js
+++ src/controllers/login/register/ProfesionalRegister.js
@@ -10,75 +10,77 @@ const {
 } = require('#src/config/cloudinaryConfig');
 const { FRONT_URL, JWT_SECRET } = require('#src/config/envConfig');
 const { sendEmailNodemailer } = require('#src/util/nodemailer');
+const User = require('#src/models/User');
 
 const profesionalRegister = async (req, res) => {
   try {
     const { password, email, dateOfBirth, ...restData } = req.body;
+
     const file = req.file.path;
+    var userResult = null;
 
-    const emailVerify = await Profesional.findOne({ email });
+    await Promise.allSettled([
+      User.findOne({ email }),
+      Profesional.findOne({ email }),
+    ]).then((settElements) => {
+      const usersMap = settElements.map((settElement, index) => {
+        if (settElement.status === 'fulfilled' && settElement.value) {
+          if (index === 0) {
+            return { user: settElement.value };
+          } else {
+            return { user: settElement.value };
+          }
+        } else {
+          return null;
+        }
+      });
+      const firstNonNullUser = usersMap.filter((user) => user)[0];
+      userResult = firstNonNullUser?.user || null; // Manejar el caso nulo
+    });
 
-    if (emailVerify) {
+    if (userResult) {
       await fs.unlink(file);
       res.status(401).json({ message: 'este email ya existe' });
     } else {
-      const hashedPass = await hashData(password);
+      const { secure_url } = await cloudinary.uploader.upload(
+        file,
+        curriculumUploadOptions,
+      );
 
-      if (!moment(dateOfBirth, 'YYYY-MM-DD', true).isValid()) {
-        await fs.unlink(file);
-        res.status(401).json({
-          message:
-            'Formato de fecha de nacimiento no válido. Porfavor usa YYYY-MM-DD.',
-        });
-      } else {
-        const today = moment();
-        const birthDate = moment(dateOfBirth);
-        const age = today.diff(birthDate, 'years', true);
+      restData.password = hashedPass;
+      restData.birthDate = dateOfBirth;
+      restData.email = email;
+      restData.curriculum = secure_url;
 
-        if (age < 18) {
-          await fs.unlink(file);
-          res.status(401).json({
-            message: 'Necesitas tener 18 años o mas para registrarte.',
-          });
-        } else {
-          const { secure_url } = await cloudinary.uploader.upload(
-            file,
-            curriculumUploadOptions,
-          );
+      const newProfesional = await Profesional.create(restData);
 
-          restData.password = hashedPass;
-          restData.birthDate = dateOfBirth;
-          restData.email = email;
-          restData.curriculum = secure_url;
+      let payload = {
+        userId: newProfesional._id,
+      };
 
-          const newProfesional = await Profesional.create(restData);
+      const tokenEmailConfirm = jwt.sign(payload, JWT_SECRET);
 
-          await fs.unlink(file);
-          let payload = {
-            userId: newProfesional._id,
-          };
+      //crear email de confirmacion
+      const url = `${FRONT_URL}/confirmar_email/${tokenEmailConfirm}`;
 
-          const tokenEmailConfirm = jwt.sign(payload, JWT_SECRET);
+      sendEmailNodemailer({
+        to: newProfesional.email,
+        subject: 'Confirmacion de cuenta - Fisium Fulness',
+        html: `
+      <p> Hola! ${newProfesional.name}, confirma la creacion de tu cuenta de Fisium Fulness</p>
+      <p> Has click en este enlace para confirmar tu cuenta:
+      <a href=${url} target="_blank"> Confirmar mi cuenta...</a></p>
+      <p> Si tu no hiciste esta peticion, ignora este mensaje.</p>`,
+      });
 
-          //crear email de confirmacion
-          const url = `${FRONT_URL}/confirmar_email/${tokenEmailConfirm}`;
+      await fs.unlink(file);
 
-          sendEmailNodemailer({
-            to: newProfesional.email,
-            subject: 'Confirmacion de cuenta - Fisium Fulness',
-            html: `
-          <p> Hola! ${newProfesional.name}, confirma la creacion de tu cuenta de Fisium Fulness</p>
-          <p> Has click en este enlace para confirmar tu cuenta:
-          <a href=${url} target="_blank"> Confirmar mi cuenta...</a></p>
-          <p> Si tu no hiciste esta peticion, ignora este mensaje.</p>`,
-          });
-
-          res.status(201).json({ message: 'creado con exito' });
-        }
-      }
+      res.status(201).json({ message: 'creado con exito' });
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res
+      .status(500)
+      .json({ message: 'Algo fallo...', errorMessage: error.message });
   }
 };
 
