c4562270934284e46ea6d16a103256083221e04c
Kevin Mamani <kevin72309@gmail.com>
2024-05-11 13:42:14 -0300

    add: streetName regex

--- src/util/regExp.js
+++ src/util/regExp.js
@@ -2,6 +2,7 @@ const phoneRegExp = /^(\+\d{1,3})?\d{10,14}$/;
 const nameRegex = /^[a-zA-Z\u00C0-\u00FF\s]*$/; // ? Only letters
 const cityRegex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]*$/;
 const latamDniExp = /^\d{7,9}$/;
+const streetNameRegex = /^(?=.*[a-zA-Z].*[a-zA-Z])[a-zA-Z0-9 ]*$/;
 const alphaNumericRegex = /^[a-zA-Z0-9]*$/;
 const numericRegex = /^[0-9]*$/;
 
@@ -10,6 +11,7 @@ module.exports = {
   nameRegex,
   cityRegex,
   latamDniExp,
+  streetNameRegex,
   alphaNumericRegex,
   numericRegex,
 };
