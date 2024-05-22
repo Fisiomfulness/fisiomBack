9dbf26e9fa1ce95680aa55e8fb228cda90542058
Kevin Mamani <kevin72309@gmail.com>
2024-05-22 16:34:45 -0300

    feat: post/update question validated with zod

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 const { errorMiddleware } = require('#src/middleware/errorMiddleware');
 const { asyncHandler } = require('../util/asyncHandler');
+const { validationMiddleware } = require('../middleware/validationMiddleware');
+const {
+  questionSchema,
+  questionResponseSchema,
+} = require('../util/validationSchemas');
 
 const {
   getAllQuestions,
@@ -12,12 +17,19 @@ const router = Router();
 
 router.get('/', asyncHandler(getAllQuestions));
 
-// ! TODO = VALIDACIÓNES ZOD - POST/PUT CON MIDDLEWARE.
 // ! TODO = QUE SOLO USUARIOS REGISTRADOS PUEDAN HACER LAS PREGUNTAS NO IMPORTA EL ROL.
-router.post('/create', asyncHandler(createQuestion));
+router.post(
+  '/create',
+  validationMiddleware(questionSchema),
+  asyncHandler(createQuestion)
+);
 
 // ! TODO = SOLO PROFESIONALES PUEDAN RESPONDER
-router.put('/response/:id', asyncHandler(respondQuestion));
+router.put(
+  '/response/:id',
+  validationMiddleware(questionResponseSchema, 'update'),
+  asyncHandler(respondQuestion)
+);
 
 // ! TODO = SOLO ADMIN Y SUPERADMIN PUEDAN BORRAR PREGUNTAS
 router.delete('/:id', (req, res) => {
--- src/util/validationSchemas.js
+++ src/util/validationSchemas.js
@@ -159,4 +159,38 @@ const commentSchema = z.object({
     ),
 });
 
-module.exports = { userSchema, professionalSchema, blogSchema, commentSchema };
+const questionSchema = z.object({
+  text: z
+    .string()
+    .min(10, 'la pregunta debe tener al menos 10 caracteres')
+    .max(500, 'la pregunta no puede tener mas de 500 caracteres'),
+  specialtyId: z
+    .string()
+    .refine(
+      async (value) => await validateId(value, 'Specialty'),
+      'especialidad no encontrada'
+    )
+    .optional(),
+});
+
+const questionResponseSchema = z.object({
+  text: z
+    .string()
+    .min(5, 'la respuesta debe tener al menos 5 caracteres')
+    .max(800, 'la respuesta no puede tener mas de 800 caracteres'),
+  professionalId: z
+    .string()
+    .refine(
+      async (value) => await validateId(value, 'Profesional'),
+      'profesional no encontrado'
+    ),
+});
+
+module.exports = {
+  userSchema,
+  professionalSchema,
+  blogSchema,
+  commentSchema,
+  questionSchema,
+  questionResponseSchema,
+};
