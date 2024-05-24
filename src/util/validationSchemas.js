9aeb6ad8deacac2140328f502e328f97886df884
Kevin Mamani <kevin72309@gmail.com>
2024-05-24 01:13:23 -0300

    fix: avoid empty questions/reponses

--- src/util/validationSchemas.js
+++ src/util/validationSchemas.js
@@ -162,6 +162,7 @@ const commentSchema = z.object({
 const questionSchema = z.object({
   text: z
     .string()
+    .trim()
     .min(10, 'la pregunta debe tener al menos 10 caracteres')
     .max(500, 'la pregunta no puede tener mas de 500 caracteres'),
   specialtyId: z
@@ -176,6 +177,7 @@ const questionSchema = z.object({
 const questionResponseSchema = z.object({
   text: z
     .string()
+    .trim()
     .min(5, 'la respuesta debe tener al menos 5 caracteres')
     .max(800, 'la respuesta no puede tener mas de 800 caracteres'),
   professionalId: z
