64f9172f6a7df8b5d3227d9c8fa637cd4b5bbdc7
Kevin Mamani <kevin72309@gmail.com>
2024-06-11 20:08:00 -0300

    refactor: service usage

--- src/controllers/user/getUserById.js
+++ src/controllers/user/getUserById.js
@@ -1,17 +1,17 @@
-const { NotFoundError } = require('#src/util/errors');
+const { findUserById } = require('#src/services/userService');
 const Profesional = require('../../models/Profesional');
 const User = require('../../models/User');
 
 const getUserById = async (req, res) => {
   const { id } = req.params;
 
-  const [user, professional] = await Promise.all([
-    User.findById(id),
-    Profesional.findById(id).populate('professionalScore'),
-  ]);
+  const foundUser = await findUserById(id);
 
-  const foundUser = user || professional;
-  if (!foundUser) throw new NotFoundError('usuario no encontrado');
+  if (foundUser instanceof User) {
+    await foundUser.populate('interests', 'name');
+  } else {
+    await foundUser.populate('professionalScore');
+  }
 
   res.status(200).json({ foundUser });
 };
