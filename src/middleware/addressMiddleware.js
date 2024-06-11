03d8caf413f4bc1513889b403f4331178f9ca15f
Kevin Mamani <kevin72309@gmail.com>
2024-06-11 19:49:30 -0300

    docs: usage with validation

--- src/middleware/addressMiddleware.js
+++ src/middleware/addressMiddleware.js
@@ -1,41 +1,46 @@
 const { geocoder } = require('../config/geocoderConfig');
 
+// * Si se va a utilizar validationMiddleware, usar este antes asi recibe el address completo en el body.
 const addressMiddleware = async (req, res, next) => {
-    try {
-        const { streetName, streetNumber, city, state, country, floorAppartment } = req.body
-        if (!streetName || !streetNumber || !city || !country) {
-            next()
-            return;
-        }
-
-        const oneLineAddress = `${streetName} ${streetNumber}, ${city}, ${state ? state + ", " : ""}${country}`
-        const result = await geocoder.geocode(oneLineAddress)
-        const bestResult = result[0]
-
-        if (bestResult 
-        && bestResult.country 
-        && bestResult.state 
-        && bestResult.city 
-        && bestResult.streetName 
-        && bestResult.streetNumber
-        && bestResult.latitude
-        && bestResult.longitude) {
-            req.body.coordinates = [bestResult.latitude, bestResult.longitude]
-        }
-        
-        req.body.address = {
-            streetName,
-            streetNumber,
-            floorAppartment,
-            city,
-            state,
-            country
-        }
-
-    } catch (err) {
-    console.log('error in geocoding middleware')
+  try {
+    const { streetName, streetNumber, city, state, country, floorAppartment } =
+      req.body;
+    if (!streetName || !streetNumber || !city || !country) {
+      next();
+      return;
     }
-  next()
-}
+
+    const oneLineAddress = `${streetName} ${streetNumber}, ${city}, ${
+      state ? state + ', ' : ''
+    }${country}`;
+    const result = await geocoder.geocode(oneLineAddress);
+    const bestResult = result[0];
+
+    if (
+      bestResult &&
+      bestResult.country &&
+      bestResult.state &&
+      bestResult.city &&
+      bestResult.streetName &&
+      bestResult.streetNumber &&
+      bestResult.latitude &&
+      bestResult.longitude
+    ) {
+      req.body.coordinates = [bestResult.latitude, bestResult.longitude];
+    }
+
+    req.body.address = {
+      streetName,
+      streetNumber,
+      floorAppartment,
+      city,
+      state,
+      country,
+    };
+  } catch (err) {
+    console.log('error in geocoding middleware');
+  }
+  next();
+};
 
 module.exports = { addressMiddleware };
