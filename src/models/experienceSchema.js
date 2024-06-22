026109182eeaccce0147e85d19091915746395c3
Kevin Mamani <kevin72309@gmail.com>
2024-06-22 12:18:52 -0300

    added some validations to subschema

--- src/models/experienceSchema.js
+++ src/models/experienceSchema.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const ObjectId = mongoose.Types.ObjectId;
 
+const currentYear = new Date().getFullYear();
+
 const experienceSchema = new Schema(
   {
     _id: {
@@ -20,18 +22,29 @@ const experienceSchema = new Schema(
       required: true,
       trim: true,
     },
-    location: {
-      type: String,
+    startDateMonth: {
+      type: Number,
       required: true,
-      trim: true,
+      min: 1,
+      max: 12,
     },
-    startDate: {
-      type: Date,
+    startDateYear: {
+      type: Number,
       required: true,
+      min: currentYear - 100,
+      max: currentYear,
     },
-    endDate: {
-      type: Date,
+    endDateMonth: {
+      type: Number,
+      min: 1,
+      max: 12,
       default: null,
+    },
+    endDateYear: {
+      type: Number,
+      default: null,
+      min: currentYear - 100,
+      max: currentYear,
     },
     description: {
       type: String,
