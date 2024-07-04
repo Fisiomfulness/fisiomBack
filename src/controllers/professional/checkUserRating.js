0912b2652316f9e23b38d97b7465d173194b8355
Kevin Mamani <kevin72309@gmail.com>
2024-07-04 20:11:33 -0300

    feat: controller to check if a user has already commented on a profile

---
+++ src/controllers/professional/checkUserRating.js
@@ -0,0 +1,15 @@
+const ProfessionalRating = require('#src/models/ProfessionalRating');
+
+// * Lo llamamos Account por que cualquier cuenta puede dejar un comentario en el perfil Profesional
+const checkUserRating = async (req, res) => {
+  const { professional_id, user_id } = req.params;
+
+  const existingRating = await ProfessionalRating.findOne({
+    _professional: professional_id,
+    _user: user_id,
+  });
+
+  res.status(200).json({ hasCommented: !!existingRating });
+};
+
+module.exports = { checkUserRating };
