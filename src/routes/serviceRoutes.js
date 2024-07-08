ccc4b3b7e7334b761f1091ada24c74ef4ed2fd82
Kevin Mamani <kevin72309@gmail.com>
2024-07-08 11:23:25 -0300

    feat: role authorization on service routes

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -4,6 +4,9 @@ const router = Router();
 const { validationMiddleware } = require('#src/middleware/validationMiddleware');
 const { errorMiddleware } = require('#src/middleware/errorMiddleware');
 const { serviceSchema } = require('#src/util/validations/index');
+const authAll = require('#src/middleware/authAll');
+const permit = require('#src/middleware/rolesMiddleware');
+const roles = require('#src/util/roles');
 
 const {
   createService,
@@ -14,15 +17,27 @@ const {
 
 router.get('/', getAllServices);
 
+router.use(authAll);
+
 router.put(
   '/:id',
+  permit(roles.PROFESSIONAL, roles.ADMIN, roles.SUPER_ADMIN),
   validationMiddleware(serviceSchema, 'update'),
   updateService
 );
 
-router.post('/', validationMiddleware(serviceSchema), createService);
+router.post(
+  '/',
+  permit(roles.PROFESSIONAL),
+  validationMiddleware(serviceSchema),
+  createService
+);
 
-router.delete('/:id', deleteService);
+router.delete(
+  '/:id',
+  permit(roles.PROFESSIONAL, roles.ADMIN, roles.SUPER_ADMIN),
+  deleteService
+);
 
 router.use(errorMiddleware);
 
