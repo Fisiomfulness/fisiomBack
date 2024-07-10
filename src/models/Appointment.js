f7fa2f8fa767733a408ce74c8a5fea769bbdf662
jackira01 <s4bu3z0@gmail.com>
2024-07-10 18:22:43 -0500

    added DEACTIVATE option to status

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -12,79 +12,85 @@ const Appointment = new Schema(
       },
     },
     _professional: {
-        type: ObjectId,
-        ref: 'Profesional',
-        required: true
+      type: ObjectId,
+      ref: 'Profesional',
+      required: true,
     },
     _patient: {
-        type: ObjectId,
-        ref: 'User',
-        required: true
+      type: ObjectId,
+      ref: 'User',
+      required: true,
     },
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    fromDateTime: {
-        type: Date,
-        required: true,
-        validate: {
-            validator: function(value) {
-                // Check if the value has a time component
-                const date = new Date(value);
-                if (
-                    date.getHours() === 0 &&
-                    date.getMinutes() === 0 &&
-                    date.getSeconds() === 0 &&
-                    date.getMilliseconds() === 0
-                ) {
-                    return false;
-                }
-                return true;
-            },
-            message: 'The appointment date must include a specific time.'
-        }
+    patientName: {
+      type: String,
+      default: '',
+      required: true,
     },
-    toDateTime: {
-        type: Date,
-        required: true,
-        validate: {
-            validator: function(value) {
-                // Check if the value has a time component
-                const date = new Date(value);
-                if (
-                    date.getHours() === 0 &&
-                    date.getMinutes() === 0 &&
-                    date.getSeconds() === 0 &&
-                    date.getMilliseconds() === 0
-                ) {
-                    return false;
-                }
-                return true;
-            },
-            message: 'The appointment date must include a specific time.'
-        }
+    additionalDescription: {
+      type: String,
+      default: '',
+    },
+    start: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          // Check if the value has a time component
+          const date = new Date(value);
+          if (
+            date.getHours() === 0 &&
+            date.getMinutes() === 0 &&
+            date.getSeconds() === 0 &&
+            date.getMilliseconds() === 0
+          ) {
+            return false;
+          }
+          return true;
+        },
+        message: 'The appointment date must include a specific time.',
+      },
+    },
+    end: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          // Check if the value has a time component
+          const date = new Date(value);
+          if (
+            date.getHours() === 0 &&
+            date.getMinutes() === 0 &&
+            date.getSeconds() === 0 &&
+            date.getMilliseconds() === 0
+          ) {
+            return false;
+          }
+          return true;
+        },
+        message: 'The appointment date must include a specific time.',
+      },
     },
     status: {
-        type: String,
-        enum: ['PENDING', 'ACCEPTED', 'REJECTED'],
-        default: 'PENDING'
+      type: String,
+      enum: ['PENDING', 'ACCEPTED', 'REJECTED', 'DEACTIVATE'],
+      default: 'PENDING',
     },
     paymentInfo: {
-        type: String,
-        default: ''
+      type: String,
+      default: '',
     },
     expiration: {
-        type: Date,
-        default: new Date(Date.now() + 3 * 60 * 60 * 1000) // 3 hours
-    }
+      type: Date,
+      default: new Date(Date.now() + 3 * 60 * 60 * 1000), // 3 hours
+    },
   },
   {
     timestamps: { createdAt: 'createdDate', updatedAt: 'updatedDate' },
-  }
+  },
 );
 
 module.exports = model('Appointment', Appointment);
-
-
-
