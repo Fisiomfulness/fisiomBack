945d67ed3f254db929826fe57c383aaf4b4e867c
brunosarti83 <brunosarti.bs@gmail.com>
2024-07-13 20:29:13 -0300

    [feat]Professionals/Users(Exact addresses are hidden)

--- src/middleware/decodeTokenUser.js
+++ src/middleware/decodeTokenUser.js
@@ -2,19 +2,16 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config/envConfig');
 
 const decodeTokenUser = (req, res, next) => {
-  const token = req.cookies?.accessToken
+  const token = req.cookies['accessToken'];
 
-  if (token) {
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
-        // if theres a problem with the cookie delete it
-        if (err) {
-            res.clearCookie('accessToken');
-        }
-        // else save the userId in the request
-        req.tokenUser = decoded;
-    });
-  }
-  next();
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.clearCookie('accessToken');
+      req.user = null;
+    }
+    req.user = decoded;
+    next();
+  });
 };
 
 module.exports = { decodeTokenUser };
