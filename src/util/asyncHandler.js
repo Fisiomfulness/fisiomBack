6da9cf3b6261793c00217c3e087795d722828e84
Kevin Mamani <kevin72309@gmail.com>
2024-08-14 12:00:57 -0300

    chore: docs in spanish

--- src/util/asyncHandler.js
+++ src/util/asyncHandler.js
@@ -1,4 +1,8 @@
-// * Using this handler we avoid try catch and next in every controller
+// * Usando este handler y el manejo de errores personalizado, se evita el try - catch - next(error) en cada controlador
+// ? USO -> En el enrutador... asyncHandler(controller);
+// * IMPORTANTE -> Se agrego libreria 'express-promise-router'
+// ? USO -> const Router = require('express-promise-router').default;
+// * Mejora la legibilidad, pero cualquiera de los dos funciona igual.
 const asyncHandler = (controller) => {
   return (req, res, next) => {
     controller(req, res, next).catch((error) => next(error));
