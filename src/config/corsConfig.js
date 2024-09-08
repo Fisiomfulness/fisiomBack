476990075655f601a1b8be0cc6ce146d09c117f9
jackira01 <s4bu3z0@gmail.com>
2024-09-08 13:07:45 -0500

    update cors logic

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,7 +1,22 @@
+const { ORIGIN_ALLOWED, FRONT_URL } = require('./envConfig');
+
 const optionCors = {
   // origin: 'http://localhost:5173',
   // ? To work with cookies needs a fix origin
-  origin: 'http://localhost:3001',
+  origin: (origin, callback) => {
+    if (!origin) {
+      // Permitir solo en desarrollo o pruebas
+      if (NODE_ENV === 'development' || NODE_ENV === 'test') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS in production'));
+      }
+    } else if (FRONT_URL == origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
