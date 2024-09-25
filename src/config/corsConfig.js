7f6e55e97e506d06b2161f1b15e0d4941840418c
FisiomUser <fisioweb17@gmail.com>
2024-09-25 13:45:04 -0500

    Update corsConfig.js

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,7 +1,20 @@
+const { FRONT_URL, NODE_ENV } = require('./envConfig');
+
 const optionCors = {
-  // origin: 'http://localhost:5173',
-  // ? To work with cookies needs a fix origin
-  origin: 'https://fisiom-front.vercel.app',
+  origin: (origin, callback) => {
+    if (!origin) {
+      // alowed only on development environment
+      if (NODE_ENV === 'development' || NODE_ENV === 'test') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS in production'));
+      }
+    } else if (FRONT_URL == origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
