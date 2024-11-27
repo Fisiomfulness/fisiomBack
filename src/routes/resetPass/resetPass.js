96b2a5d77a4a409d354723a359e3be7f575ecf10
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2024-11-27 09:20:16 -0500

    Reestructuracion cambio contraseña

--- src/routes/resetPass/resetPass.js
+++ src/routes/resetPass/resetPass.js
@@ -1,8 +1,11 @@
 const {indexNewPassword} = require('#src/controllers/newPassword/indexnew_Pass')
+const {changePassword} = require('#src/controller/newPassword/changePassword')
 const express = require('express')
 const router = express.Router()
 
     
-router.post("/reserPassword", indexNewPassword);
+router.post("/resertPassword", indexNewPassword);
+router.post("/changePassword", changePassword);
+
 
 module.export= router
