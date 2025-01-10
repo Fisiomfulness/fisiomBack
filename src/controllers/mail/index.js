362e6224d13c2bd0ac1f3a87849f3d3787355aee
EdgarFav <edgarfda17@gmail.com>
2025-01-09 21:43:32 -0600

    Feat: creating a instance of nodemailer to request info from the client and creating te route POST /mail/request-info

--- src/controllers/mail/index.js
+++ src/controllers/mail/index.js
@@ -2,11 +2,13 @@ const { sendEmail } = require('./sendMail');
 const { sendBlogNotification } = require('./sendBlogNotification');
 const { sendProfessionalNotification } = require('./sendProfesionalNotification');
 const { sendEmailRequestingBudget } = require('./enterpriseRequestBudget');
+const { sendEmailRequestingInfo } = require('./requestInfo');
 
 module.exports = {
   sendEmail,
   sendProfessionalNotification,
   sendBlogNotification,
-  sendEmailRequestingBudget
+  sendEmailRequestingBudget,
+  sendEmailRequestingInfo,
 };
 
